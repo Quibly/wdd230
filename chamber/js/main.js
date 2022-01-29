3c0655a5a13b4262e8fa01b844805d8331f840b4
Quibly <trippy524@yahoo.com>
2022-01-29 12:36:54 -0700

    added header/footer html and java

---
+++ chamber/js/main.js
@@ -0,0 +1,15 @@
+const datefield = document.querySelector("date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("menuBtn").classList.toggle("open");
+}
+const x = document.getElementById('menuBtn');
+x.onclick = toggleMenu;
+
+document.querySelector("#copyright").textContent = `©${new Date().getFullYear()} Vernal Chamber`;

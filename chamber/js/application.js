cd2fe4278546d0110db9274ad306224fbfd42e40
Quibly <trippy524@yahoo.com>
2022-02-25 01:07:25 -0700

    fixed date input field

--- chamber/js/application.js
+++ chamber/js/application.js
@@ -1,10 +1,3 @@
-const datehidden = document.querySelector("#dateHidden");
-
-function dateTime() {
-    const dateTimeValue = new Date($.now());
-    datehidden.setAttribute('value', dateTimeValue);
-    
-}
-
-const applicationBtn = document.querySelector('#applicationBtn');
-applicationBtn.addEventListener('click', function() {dateTime()});
+let datehidden = document.querySelector("#dateHidden");
+let date = new Date();
+datehidden.setAttribute('value', date);

872219087d33a318165fc5de590fd2f798139965
Quibly <trippy524@yahoo.com>
2022-03-05 11:47:04 -0700

    updated js

--- chamber/js/viewToggle.js
+++ chamber/js/viewToggle.js
@@ -2,29 +2,25 @@ let listBtn = document.querySelector('#listBtn');
 let gridBtn = document.querySelector('#gridBtn')
 let listGridId = document.querySelector('.cards');
 let intViewportWidth = parseInt(visualViewport.width);
-console.log(intViewportWidth);
 
 function checkSize() {
     intViewportWidth = parseInt(visualViewport.width);
     if ((intViewportWidth < 900) && (intViewportWidth > 550)) {
         listGridId.setAttribute('id', 'listView');
-        console.log('step 1');
     } else if (intViewportWidth < 550) {
         listGridId.setAttribute('id', '');
-        console.log('step 2');
     } else {
         listGridId.setAttribute('id', '');
-        console.log('step 3');
     }
 }
 
 function toggleGrid() {
         listGridId.setAttribute('id', '');
-        console.log(gridBtn.value);
+        
 }
 function toggleList() {
         listGridId.setAttribute('id', 'listView');
-        console.log(listBtn.value);
+        
 }
 
 checkSize();

38eed949a5fb2f4398045c97f29678cd2aaca2b2
Quibly <trippy524@yahoo.com>
2022-03-26 22:41:31 -0600

    updated menu zindex

--- tis/js/main.js
+++ tis/js/main.js
@@ -16,3 +16,11 @@ function toggleMenu() {
     navBtn.classList.toggle("open");
     navList.classList.toggle("open");
 }
+
+// For routing button clicks for reservations to the reservations page
+let reservationsLink = document.querySelector('.reservationsLink');
+reservationsLink.onclick = linkToReservations;
+
+function linkToReservations() {
+    window.location.href='https://quibly.github.io/wdd230/tis/html/reservations.html';
+}

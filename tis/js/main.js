f8c01fc885a2302de14e504141a5754a1b209b86
Quibly <trippy524@yahoo.com>
2022-03-26 19:24:19 -0600

    Started Temple Site Home page

---
+++ tis/js/main.js
@@ -0,0 +1,18 @@
+// For Date display at top of page
+let now = new Date();
+let date = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+let dateBanner = document.querySelector('#dateBanner');
+dateBanner.textContent = date;
+
+// For display of last modification date in footer
+document.querySelector("#footerCopy").textContent = `Copyright 2022 | Adam Dutson | WDD 230 | Last Modification: ${document.lastModified}`;
+
+// For view toggle of navigation menu
+let navBtn = document.querySelector('#navBtn');
+let navList = document.querySelector('#navList');
+navBtn.onclick = toggleMenu;
+
+function toggleMenu() {
+    navBtn.classList.toggle("open");
+    navList.classList.toggle("open");
+}
